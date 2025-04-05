57383904481229e943c2cabe80eba5ab233180f9
matejkoo217 <nostalmcserver@gmail.com>
2025-04-05 21:12:10 +0200

    Added support for player displaynames

--- src/com/johnymuffin/beta/discordchatbridge/DCBGameListener.java
+++ src/com/johnymuffin/beta/discordchatbridge/DCBGameListener.java
@@ -20,7 +20,12 @@ public class DCBGameListener extends PlayerListener {
     @Override
     public void onPlayerJoin(PlayerJoinEvent event) {
         String chatMessage = plugin.getConfig().getConfigString("message.join-message");
-        chatMessage = chatMessage.replace("%username%", event.getPlayer().getName());
+        if (plugin.getConfig().getConfigBoolean("message.use-displayname")) {
+        	chatMessage = chatMessage.replace("%username%", sanitizeDisplayName(event.getPlayer().getDisplayName()));
+        }
+        else {
+            chatMessage = chatMessage.replace("%username%", event.getPlayer().getName());
+        }
         chatMessage = chatMessage.replace("%onlineCount%", String.valueOf(Bukkit.getServer().getOnlinePlayers().length));
         chatMessage = chatMessage.replace("%maxCount%", String.valueOf(Bukkit.getServer().getMaxPlayers()));
         plugin.getDiscordCore().getDiscordBot().discordSendToChannel(plugin.getConfig().getConfigString("channel-id"), chatMessage);
@@ -29,7 +34,12 @@ public class DCBGameListener extends PlayerListener {
     @Override
     public void onPlayerQuit(PlayerQuitEvent event) {
         String chatMessage = plugin.getConfig().getConfigString("message.quit-message");
-        chatMessage = chatMessage.replace("%username%", event.getPlayer().getName());
+        if (plugin.getConfig().getConfigBoolean("message.use-displayname")) {
+        	chatMessage = chatMessage.replace("%username%", sanitizeDisplayName(event.getPlayer().getDisplayName()));
+        }
+        else {
+        	chatMessage = chatMessage.replace("%username%", event.getPlayer().getName());
+        }
         chatMessage = chatMessage.replace("%onlineCount%", String.valueOf(Bukkit.getServer().getOnlinePlayers().length - 1));
         chatMessage = chatMessage.replace("%maxCount%", String.valueOf(Bukkit.getServer().getMaxPlayers()));
         plugin.getDiscordCore().getDiscordBot().discordSendToChannel(plugin.getConfig().getConfigString("channel-id"), chatMessage);
@@ -43,7 +53,12 @@ public class DCBGameListener extends PlayerListener {
 
         if (plugin.getConfig().getConfigBoolean("webhook.use-webhook")) {
             final DiscordWebhook webhookMessage = new DiscordWebhook(plugin.getConfig().getConfigString("webhook.url"));
-            webhookMessage.setUsername(event.getPlayer().getName());
+            if (plugin.getConfig().getConfigBoolean("message.use-displayname")) {
+            	webhookMessage.setUsername(sanitizeDisplayName(event.getPlayer().getDisplayName()));
+            }
+            else {
+                webhookMessage.setUsername(event.getPlayer().getName());
+            }
             webhookMessage.setContent(sanitizeMessage(event.getMessage()));
             webhookMessage.setAvatarUrl("http://minotar.net/helm/" + event.getPlayer().getName() + "/100.png");
             webhookMessage.setTts(false);
@@ -57,17 +72,29 @@ public class DCBGameListener extends PlayerListener {
 
         } else {
             String chatMessage = plugin.getConfig().getConfigString("message.game-chat-message");
-            chatMessage = chatMessage.replace("%messageAuthor%", event.getPlayer().getName());
-            chatMessage = chatMessage.replace("%message%", event.getMessage());
-            chatMessage = sanitizeMessage(chatMessage);
+            if (plugin.getConfig().getConfigBoolean("message.use-displayname")) {
+            	chatMessage = chatMessage.replace("%messageAuthor%", sanitizeDisplayName(event.getPlayer().getDisplayName()));
+            }
+            else {
+                chatMessage = chatMessage.replace("%messageAuthor%", event.getPlayer().getName());
+            }
+            chatMessage = chatMessage.replace("%message%", sanitizeMessage(event.getMessage()));
             plugin.getDiscordCore().getDiscordBot().discordSendToChannel(plugin.getConfig().getConfigString("channel-id"), chatMessage);
         }
     }
 
+    // Method to sanitize player display name, removing Minecraft color codes and formatting
+    public String sanitizeDisplayName(String displayName) {
+        if (displayName == null) return "";
+        return displayName.replaceAll("§[0-9a-fA-Fklmnor]", "").trim();    // Remove color codes and formatting codes (e.g., §c, §f, §l, §r, etc.)
+    }
+
+    // Sanitizing method to clean up the message (removing @everyone, @here, and Minecraft color codes)
     public String sanitizeMessage(String chatMessage) {
-        chatMessage = chatMessage.replaceAll(Pattern.quote("@"), " ");
+        chatMessage = chatMessage.replaceAll("§[0-9a-fA-Fklmnor]", "");
         chatMessage = chatMessage.replaceAll(Pattern.quote("@everyone"), " ");
         chatMessage = chatMessage.replaceAll(Pattern.quote("@here"), " ");
+        chatMessage = chatMessage.replaceAll(Pattern.quote("@"), " ");
         return chatMessage;
     }
 
